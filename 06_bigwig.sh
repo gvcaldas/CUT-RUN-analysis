6023ad5acf5421b1ae0e4479741c15f0e5642043
Gina Caldas <81447801+gvcaldas@users.noreply.github.com>
2023-02-02 23:54:18 -0800

    Update 06_bigwig.sh

--- 06_bigwig.sh
+++ 06_bigwig.sh
@@ -11,8 +11,34 @@
 		#reads per basepair x fragment length = # of reads expected for every chunk of Xbasepairs (fragment)
 # RPGC = observed number of reads per bin / Denominator = ratio of actual reads in that bin normalized to expected number of reads per that bin size. 
 
-
 ## this is effective genome size for gh38: 2913022398
+
+# Important Settings:
+--ignoreDuplicates (some papers suggest to not do this for C&R or C&T data) : If set, reads that have the same orientation and start position will be considered only once. If reads are paired, the mate’s position also has to coincide to ignore a read. I believe that is OK to work with duplicates, as long as your library is not high on duplicates. For me, >40% duplicates is too much and I would prefer to ignore. But overall, I suggest to get a sense of your data by comparing removing duplicates or not.
+
+--minMappingQuality :A mapping quality of 10 or less indicates that there is at least a 1 in 10 chance that the read truly originated elsewhere.
+
+--centerReads : reads are centered with respect to the fragment length. For paired-end data, the read is centered at the fragment length defined by the two ends of the fragment.
+
+--extendReads : Paired-end: Reads with mates are always extended to match the fragment size defined by the two read mates. Unmated reads, mate reads that map too far apart (>4x fragment length) or even map to different chromosomes are treated like single-end reads. The input of a fragment length value is optional. If no value is specified, it is estimated from the data (mean of the fragment size of all mate reads).
+
+--smoothLength : The smooth length defines a window, larger than the binSize, to average the number of reads. For example, if the –binSize is set to 20 and the –smoothLength is set to 60, then, for each bin, the average of the bin and its left and right neighbors is considered.
+
+## Additional parameters that I tested and seemed to work best: 
+
+--minFragmentLength : The minimum fragment length needed for read/pair inclusion. (Default: 0)
+
+--maxFragmentLength : The maximum fragment length needed for read/pair inclusion. (Default: 0)
+
+## The user provided fragment length is only used as a fallback for singletons or mate reads that map too far apart (with a distance greater than four times the fragment length or are located on different chromosomes).
+
+# Note: I have been processing with BinSize = 20, potentially, I could speed up with binsize = 50 , Plus --smooth length could be 100 or 150. However, Binsize 20 gives better resolution and I don't think smooth of 60 affects has an impact on the data.  
+
+# in summary, these are the settings I think are the best. For data already filtered for discordant pairs.
+
+bamCoverage -b file.st.bam --ignoreDuplicates --minMappingQuality 10 --centerReads --extendReads 200 --minFragmentLength 100 --maxFragmentLength 250 --binSize 20 --smoothLength 60 --normalizeUsing RPGC --effectiveGenomeSize 2913022398 -o file.bw
+
+
 
 ## example to batch: could be run as a loop.
 
